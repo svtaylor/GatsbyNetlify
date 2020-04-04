d54e8d90a0e53ead0702921e0c1483b65b87657b
Steve Taylor <thesaint05@gmail.com>
2020-04-03 19:06:07 -0600

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     // edit below
     title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    author: `Steve Taylor`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
